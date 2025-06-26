c0224b3da20f30d74f3059d1c4fae5b954c46446
jatulya <atulyajayakrishnan@gmail.com>
2025-06-26 16:54:56 +0530

    Done using bloc consumer.

--- my_first_app/lib/login.dart
+++ my_first_app/lib/login.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:my_first_app/bloc/login_bloc.dart';
 import 'package:my_first_app/bloc/login_event.dart';
-import 'package:my_first_app/cubit/login_cubit.dart';
 import 'package:my_first_app/bloc/login_state.dart';
-
 
 class Arguements {
   String email;
@@ -23,129 +22,144 @@ class LoginForm extends StatefulWidget{
 class _LoginPageState extends State<LoginForm> {
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+Widget build(BuildContext context) {
+  return BlocProvider(
+    create: (_) => LoginBloc(),
+    child: Scaffold(
       backgroundColor: Colors.black,
-      body: BlocListener<LoginBloc, LoginState>(
+      body: BlocConsumer<LoginBloc, LoginState>(
         listenWhen: (prev, curr) => prev.enabled != curr.enabled && curr.enabled,
         listener: (context, state) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('Form is now valid!')),
-          );
         },
-        child : SizedBox(
-        height: 1000,
-        child: ListView(
-          children: [
-            Container(
-              height: 120,
-              padding: EdgeInsets.only(top: 30),
-              child: Padding(
-                padding: EdgeInsets.only(left: 10),
-                child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Enter Your Credentials',
-                    style: TextStyle(fontSize: 25, color: Colors.white70),
+        buildWhen: (prev, curr) => prev.enabled != curr.enabled,
+        builder: (context, state) {
+          return SizedBox(
+            height: 1000,
+            child: ListView(
+              children: [
+                Container(
+                  height: 120,
+                  padding: EdgeInsets.only(top: 30),
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Enter Your Credentials',
+                          style: TextStyle(fontSize: 25, color: Colors.white70),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          "You'll use this email to make and track investments",
+                          style: TextStyle(fontSize: 12, color: Colors.white30),
+                        ),
+                      ],
+                    ),
                   ),
-                  SizedBox(height: 4),
+                ),
 
-                  Text(
-                    "You'll use this email to make and track investments",
-                    style: TextStyle(fontSize: 12, color: Colors.white30),
-                  ),
-                ],
-              ),
-              ) 
-            ),             
+                SizedBox(height: 80),
 
-            SizedBox(height: 80,),
-
-            Container(
-              height: 210,
-              padding: EdgeInsets.only(left: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('EMAIL', style: TextStyle(color: Colors.white60),),                 
-                  SizedBox(height: 10,),
-
-                  TextField(
-                    cursorColor: Colors.green[200],
-
-                    style: TextStyle(color: Colors.green[300]),
-
-                    decoration: InputDecoration(
-                      hintText: "firstname@domain.com",
-                      hintStyle: TextStyle(color: Colors.green[200]),
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.green),
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.green, width: 2),
+                Container(
+                  height: 210,
+                  padding: EdgeInsets.only(left: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('EMAIL', style: TextStyle(color: Colors.white60)),
+                      
+                      SizedBox(height: 10),
+                      
+                      TextField(
+                        cursorColor: Colors.green[200],
+                        style: TextStyle(color: Colors.green[300]),
+                        decoration: InputDecoration(
+                          hintText: "firstname@domain.com",
+                          hintStyle: TextStyle(
+                              color: const Color.fromARGB(255, 152, 179, 153)),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.green, width: 2),
+                          ),
+                        ),
+                        onChanged: (text) {
+                          context.read<LoginBloc>().add(EmailChanged(text));
+                        },
                       ),
                       
-                    ),
-                    onChanged: (text){
-                      context.read<LoginBloc>().add(EmailChanged(text));
-                    }
+                      SizedBox(height: 20),
+                      
+                      Text('PASSWORD', style: TextStyle(color: Colors.white60)),
+                      
+                      SizedBox(height: 10),
+                      
+                      TextField(
+                        cursorColor: Colors.green[200],
+                        style: TextStyle(color: Colors.green[300]),
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Enter your  password",
+                          hintStyle: TextStyle(
+                              color: const Color.fromARGB(255, 152, 179, 153)),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.green, width: 2),
+                          ),
+                        ),
+                        onChanged: (text) {
+                          context
+                              .read<LoginBloc>()
+                              .add(PasswordChanged(text));
+                        },
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 20,),
-                  
-                  Text('PASSWORD', style: TextStyle(color: Colors.white60),),
-                  SizedBox(height: 10,),
+                ),
 
-                  TextField(
-                    cursorColor: Colors.green[200],
-                    style: TextStyle(color: Colors.green[300]),
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Enter your  password",
-                      hintStyle: TextStyle(color: Colors.green[200]),
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green),
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green, width: 2),
-                      ),
+                SizedBox(height: 200),
+
+                Visibility(
+                  visible: state.enabled,
+                  child: FilledButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                          WidgetStatePropertyAll<Color>(Colors.green),
                     ),
-                    onChanged: (text){
-                      context.read<LoginBloc>().add(PasswordChanged(text));
-                    }
-                  ),
-                ],
-              ),
-            ),
-
-            SizedBox(height: 200,),
-            BlocBuilder<LoginBloc, LoginState>(
-                buildWhen: (prev, curr) => prev.enabled != curr.enabled,
-                builder: (context, state) {
-                  return Visibility(
-                    visible: state.enabled,
-                    child: FilledButton(
-                      style: ButtonStyle(
-                        backgroundColor: WidgetStatePropertyAll<Color>(Colors.green),
-                      ),
-                      onPressed: () {
-                        Navigator.pushNamed(
-                          context,
-                          '/logged-in',
-                          arguments: Arguements(state.email, state.password),
+                    onPressed: () {
+                      if (state.email != 'atulya@gmail.com') {
+                        Fluttertoast.showToast(
+                          msg: "Wrong credentials. Type again",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.BOTTOM,
+                          backgroundColor: Colors.redAccent,
+                          textColor: Colors.white,
                         );
-                      },
-                      child: Text('Login'),
-                    ),
-                  );
-                }
-            )
-          ]
-        )
-        
+                        return;
+                      }
+                      Navigator.pushNamed(
+                        context,
+                        '/logged-in',
+                        arguments: Arguements(state.email, state.password),
+                      );
+                    },
+                    child: Text('Login'),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
-    )
-    );
-  }
+    ),
+  );
+}
+
 }
 
