9446ae0371835045cdba7c259b59bf24092c0f4a
J ATULYA <109218299+jatulya@users.noreply.github.com>
2025-07-16 17:25:32 +0530

    fix: injected dio, method return type changed to future

--- my_first_app/lib/services/movie_services.dart
+++ my_first_app/lib/services/movie_services.dart
@@ -1,27 +1,31 @@
-// movie_service.dart
+
 import 'package:dio/dio.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:injectable/injectable.dart';
+import 'package:my_first_app/cubit/movie/injectable.dart';
 import '../models.dart';
 
+@Injectable()
 class MovieService {
-  final Dio _dio = Dio();
-  // final String _apiKey = dotenv.env['API_KEY'] ?? '';
+  MovieService();  
+
+  final Dio _dio = getIt<Dio>();
 
   Future<Movie> fetchMovieByTitle(String title) async {
-    final response = await _dio.get(
-      'http://www.omdbapi.com/',
-      queryParameters: {
-        't': title,
-        'apikey': 'd3e1282b',
-      },
-    );
+  
+      final response = await _dio.get(
+        'http://www.omdbapi.com/',
+        queryParameters: {'t': title, 'apikey': ''},
+      );
 
-    final data = response.data;
+      final data =  response.data as Map<String, dynamic>;
+      print("Log from myself : Services : $data");
 
-    if (data['Response'] == 'False') {
-      throw Exception(data['Error']);
-    }
+      if (data['Response'] == 'False') {
+        throw Exception(data['Error']);
+      }
 
-    return Movie.fromJson(data);
+      print("Log from myself : Services : Movie after truncate : ${Movie.fromJson(data).genre}");
+      return Movie.fromJson(data);
+    
   }
 }
