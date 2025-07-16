dece92959008a503aa8181559f7af0b24bb8c819
J ATULYA <109218299+jatulya@users.noreply.github.com>
2025-07-16 17:34:50 +0530

    Update injectable.dart

--- my_first_app/lib/cubit/movie/injectable.dart
+++ my_first_app/lib/cubit/movie/injectable.dart
@@ -1,18 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
-import 'package:my_first_app/cubit/movie/injectable.config.dart';
-
+import 'package:my_first_app/cubit/movie/injectable.config.dart'; //run build_runner to genrate this file
 
 final getIt = GetIt.instance;
 
 @InjectableInit()
-Future<void> configureInjections() async { 
-  print("Log from myself : Entered config"); 
-  getIt.init();
-  print("Log from myself : Instance created.");
-  }
-
+Future<void> configureInjections() async => getIt.init();
+  
 @module
 abstract class RegisterModule {
   @lazySingleton
