4fdeb381caf4c08f03f0b54951131490d5c109ee
J ATULYA <109218299+jatulya@users.noreply.github.com>
2025-07-16 17:29:24 +0530

    Refactor: repositroy injected.

--- my_first_app/lib/cubit/movie/movie_cubit.dart
+++ my_first_app/lib/cubit/movie/movie_cubit.dart
@@ -1,20 +1,19 @@
-
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+import 'package:my_first_app/cubit/movie/injectable.dart';
 import 'package:my_first_app/cubit/movie/movie_state.dart';
-import 'package:my_first_app/models.dart';
 import 'package:my_first_app/repositories/movie_repositories.dart';
 
-
+@Injectable()
 class MovieCubit extends Cubit<MovieState> {
-  final MovieRepository repository;
-
-  MovieCubit(this.repository) : super(MovieState.initial());
+  final IMovieRepository repository = getIt<IMovieRepository>();
+  MovieCubit() : super(MovieState.initial());
 
   void loadMovie(String title) async {
     emit(MovieState(loading: true));
     try {
-      final movie = await repository.getMovieByTitle(title);
-      emit(MovieState(movie: movie));
+      final movieInfo = await repository.getMovieByTitle(title);
+      emit(MovieState(movie: movieInfo));
     } catch (e) {
       emit(MovieState(error: e.toString()));
     }
