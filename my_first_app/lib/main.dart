6553a92d40b3d7ccdc870bd7dac84cbb5f225dbf
J ATULYA <109218299+jatulya@users.noreply.github.com>
2025-07-16 17:23:21 +0530

    Add: sentry, configure injections

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -1,35 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:my_first_app/cubit/movie/injectable.dart';
 
-import 'package:my_first_app/cubit/movie/movie_locator.dart';
 import 'package:my_first_app/pages/loggedin.dart';
 import 'package:my_first_app/pages/login.dart';
 import 'package:my_first_app/pages/movie_page.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // try {
-  //   dotenv.load(fileName: ".env"); 
-  // } 
-  // catch (e){
-  //   debugPrint("Error ******** $e");
-  // }
-  setupLocator();
-  runApp(const MyApp());
+ 
+  await dotenv.load(fileName: '.env');
+  await configureInjections();
+
+  final dsn = dotenv.maybeGet('SENTRY_TOKEN');
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = dsn;
+      options.sendDefaultPii = dotenv.getBool(
+        'SENTRY_DEFAULT_PII',
+        fallback: false,
+      );
+    },
+    appRunner: () {
+      runApp(SentryWidget(child: const MyApp()));
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp ({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home : LoginForm(),
-        title: 'Login PAge Demo',
-        routes: {
-          '/logged-in' : (context) => LoggedInPage(),
-          '/movie-page' : (context) => MoviePage()
-        },
-      ); 
+      home: LoginForm(),
+      title: 'Login PAge Demo',
+      routes: {
+        '/logged-in': (context) => LoggedInPage(),
+        '/movie-page': (context) => MoviePage(),
+      },
+    );
   }
 }
