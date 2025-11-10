83497a8b1e1c0904553dd926029463612cf3bf11
jatulya <atulyajayakrishnan@gmail.com>
2025-11-10 17:30:24 +0530

    feat:3D pushable button

--- practice_flutter/lib/app/modules/homescreen.dart
+++ practice_flutter/lib/app/modules/homescreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:practice_flutter/app/common/constants/constant_values.dart';
 import 'package:practice_flutter/app/common/exports.dart';
 import 'package:practice_flutter/app/common/extensions.dart';
+import 'package:practice_flutter/app/common/widgets/pushable_button.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -13,38 +14,33 @@ class HomeScreen extends StatelessWidget {
         title: Text(AppStrings.appTitle.translate()),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            // Heading
-            Text(AppStrings.home.translate(), style: AppTextStyles.title46),
-            const SizedBox(height: 24),
-            // Button
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: homeButtons.length,
-              itemBuilder: (context, index) {
-                final item = homeButtons[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 8.0,
-                    horizontal: 16.0,
-                  ),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, item.buttonNavigation);
-                    },
-                    child: Text(
-                      item.buttonName,
-                      style: AppTextStyles.lightBody,
-                    ),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // Heading
+          Text(AppStrings.home.translate(), style: AppTextStyles.title46),
+          const SizedBox(height: AppSpacing.lg),
+          // Button
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: homeButtons.length,
+            itemBuilder: (context, index) {
+              final item = homeButtons[index];
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: AppSpacing.sm,
+                  horizontal: AppSpacing.md,
+                ),
+                child: PushableButton(
+                  text: item.buttonName,
+                  onPressed: () {
+                    Navigator.pushNamed(context, item.buttonNavigation);
+                  },
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
