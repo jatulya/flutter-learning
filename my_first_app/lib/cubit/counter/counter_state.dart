3a4dfcdc0e5990c41036f2ae9a66671c04c99865
J-Atulya <atulyajayakrishnan@gmail.com>
2026-01-25 12:36:35 +0530

    refactor: deleted unwanted apps

--- my_first_app/lib/cubit/counter/counter_state.dart
+++
@@ -1,8 +0,0 @@
-class CounterState {
-  final int seats;
-
-  CounterState(this.seats);
-
-  bool get enabledSubmit => seats > 0;
-  bool get isMaxExceeded => seats > 5;
-}
--- my_first_app/lib/cubit/loggedin/logged_in_state.dart
+++
@@ -1,6 +0,0 @@
-class LoggedInState {
-  final String sender;
-
-  LoggedInState(this.sender);
-  
-}
